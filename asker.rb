fcb4de2b860ddab2d07d5874ca45b2f7fd85f347
odionman <ttkoshy@gmail.com>
2019-01-16 20:04:44 -0500

    this is where ruby starts

---
+++ asker.rb
@@ -0,0 +1,10 @@
+puts "Give me a person: "
+person = gets.chomp()
+
+puts "Give me a noun: "
+noun = gets.chomp()
+
+puts "Give me a verb: "
+verb = gets.chomp()
+
+print (person + " "  + verb + " "  + noun + " ")
